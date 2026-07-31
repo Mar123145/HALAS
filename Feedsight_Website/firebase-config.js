8920734a3106009ab65e456ab5b54fc11a5137f6
Mar123145 <cantilloja@students.nu-fairview.edu.ph>
2026-07-31 20:46:26 +0800

    Add files via upload

---
+++ Feedsight_Website/firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB4kMSArxqKUSdnyAx4k_gAlRC2FyBfHdk",
+    authDomain: "feedsight-41a1f.firebaseapp.com",
+    databaseURL: "https://feedsight-41a1f-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "feedsight-41a1f",
+    storageBucket: "feedsight-41a1f.firebasestorage.app",
+    messagingSenderId: "739424992190",
+    appId: "1:739424992190:web:aaadbfd0035938f891d7ef",
+    measurementId: "G-K0YZ50DJ9X"
+};
